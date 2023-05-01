2b01619022d59bedf96b5995c0cc877959c17d49
developer <i.svirin@nordavind.ru>
2023-05-01 05:34:44 +0000

    Complete implementation (untested)

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,7 +1,8 @@
 import
 {
     BigInt,
-    dataSource
+    dataSource,
+    store
 } from "@graphprotocol/graph-ts"
 import
 {
@@ -97,33 +98,54 @@ export function handleDisputeRejected(event: DisputeRejected): void
 
 export function handleArbitratorAdded(event: ArbitratorAdded): void
 {
-
+    let a = new Arbitrator(event.params.arbAddr.toString());
+    a.manager = event.params.manager;
+    a.name = event.params.name;
+    a.save();
 }
 
 export function handleArbitratorDeleted(event: ArbitratorDeleted): void
 {
+    store.remove("Arbitrator", event.params.arbAddr.toString());
 }
 
 export function handleArbitratorManagerChanged(event: ArbitratorManagerChanged): void
 {
+    let a = Arbitrator.load(event.params.arbAddr.toString()) as Arbitrator;
+    a.manager = event.params.manager;
+    a.save();
 }
 
 export function handleContractAdded(event: ContractAdded): void
 {
+    let c = new Contract(event.params.arbAddr.toString()+"-"+event.params.contr.toString());
+    c.contract = event.params.contr;
+    c.arbitrator = event.params.arbAddr.toString();
+    c.save();
 }
 
 export function handleContractRemoved(event: ContractRemoved): void
 {
+    store.remove("Contract", event.params.arbAddr.toString()+"-"+event.params.contr.toString());
 }
 
 export function handleOperationAdded(event: OperationAdded): void
 {
+    let o = new Operation(event.params.arbAddr.toString()+"-"+event.params.operation.toString());
+    o.operation = event.params.operation;
+    o.extraData = event.params.extraData;
+    o.arbitrator = event.params.arbAddr.toString();
+    o.save();
 }
 
 export function handleOperationChanged(event: OperationChanged): void
 {
+    let o = Operation.load(event.params.arbAddr.toString()+"-"+event.params.operation.toString()) as Operation;
+    o.extraData = event.params.extraData;
+    o.save();
 }
 
 export function handleOperationRemoved(event: OperationRemoved): void
 {
+    store.remove("Operation", event.params.arbAddr.toString()+"-"+event.params.operation.toString());
 }
